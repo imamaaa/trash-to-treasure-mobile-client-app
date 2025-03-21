ad57b61247cc91b4bef81fc5c99a84a87dbddf9a
Imama Amjad <112810800+imamaaa@users.noreply.github.com>
2025-03-21 08:51:30 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:myapp/sign_in_page.dart';
-import 'firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // Import dotenv
 import 'package:overlay_support/overlay_support.dart';
+import 'firebase_options.dart'; // Import Firebase options
 import 'splash_screen.dart';
 import 'profilePage.dart';
 import 'scan_qr.dart';
@@ -14,10 +14,19 @@ import 'challenges.dart';
 import 'my_badges.dart';
 import 'enter_pin.dart';
 import 'stores.dart';
+import 'sign_in_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Load environment variables
+  await dotenv.load();
+
+  // Initialize Firebase securely
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
@@ -25,29 +34,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return OverlaySupport(
-    child: MaterialApp(
-      routes: {
-        '/profile': (context) => ProfilePage(),
-        '/scan_qr': (context) => QRScannerPage(),
-        '/settings': (context) => SettingsPage(),
-        '/history': (context) => HistoryPage(),
-        '/wallet': (context) => WalletPage(),
-        '/signinpage': (context) => SignInPage(),
-        '/feedback': (context) => FeedbackPage(),
-        '/challenges':(context) => ChallengesPage(),
-        '/my_badges':(context) => MyBadgesPage(),
-        '/enter_pin':(context) => EnterPINCodePage(),
-        '/stores':(context) => StoresPage(),
-
-
-      },
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      child: MaterialApp(
+        routes: {
+          '/profile': (context) => ProfilePage(),
+          '/scan_qr': (context) => QRScannerPage(),
+          '/settings': (context) => SettingsPage(),
+          '/history': (context) => HistoryPage(),
+          '/wallet': (context) => WalletPage(),
+          '/signinpage': (context) => SignInPage(),
+          '/feedback': (context) => FeedbackPage(),
+          '/challenges': (context) => ChallengesPage(),
+          '/my_badges': (context) => MyBadgesPage(),
+          '/enter_pin': (context) => EnterPINCodePage(),
+          '/stores': (context) => StoresPage(),
+        },
+        title: 'Trash to Treasure',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: SplashScreen(),
       ),
-      home: SplashScreen(),
-    )
     );
   }
 }
